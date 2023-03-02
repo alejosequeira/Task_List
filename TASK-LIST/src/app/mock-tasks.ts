03e5cdf33cf75e913b4e8d036402fca6c947fa64
alejosequeira <sequeiraar@gmail.com>
2023-03-02 18:12:50 -0300

    servicio y servidor de tareas listo

--- TASK-LIST/src/app/mock-tasks.ts
+++ TASK-LIST/src/app/mock-tasks.ts
@@ -4,25 +4,25 @@ export const TASKS: Task[]= [
     {
         id:1,
         text: "Terminar primer modulo de Angular",
-        day: 'Agosto 5 a las 10:00',
+        day: "Agosto 5 a las 10:00",
         reminder: true
     },
     {
         id:2,
         text: "Terminar segundo modulo de Angular",
-        day: 'Agosto 6 a las 10:00',
+        day: "Agosto 6 a las 10:00",
         reminder: true
     },
     {
         id:3,
         text: "Terminar tercer modulo de Angular",
-        day: 'Agosto 7 a las 10:00',
-        reminder: true
+        day: "Agosto 7 a las 10:00",
+        reminder: false
     },
     {
         id:4,
-        text: "Terminar cuarto modulo de Angular",
-        day: 'Agosto 8 a las 10:00',
+    text: "Terminar cuarto modulo de Angular",
+        day: "Agosto 8 a las 10:00",
         reminder: true
     },
 ]
